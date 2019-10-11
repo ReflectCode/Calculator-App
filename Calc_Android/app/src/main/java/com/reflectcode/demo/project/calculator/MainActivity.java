ebeab6dd63c130570eaccee9f30fc276d2f2375b
Reflect Code <reflectcodelabs@gmail.com>
2019-10-12 01:35:15 +0530

    Calculator App

--- Calc_Android/app/src/main/java/com/reflectcode/demo/project/calculator/MainActivity.java
+++ Calc_Android/app/src/main/java/com/reflectcode/demo/project/calculator/MainActivity.java
@@ -1,3 +1,23 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2019 Reflect Code Technologies (OPC) Pvt. Ltd. (http://ReflectCode.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 package com.reflectcode.demo.project.calculator;
 
 import android.support.v7.app.AppCompatActivity;
@@ -10,7 +30,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
 
     Button one, two, three, four, five, six, seven, eight, nine, zero, add, sub, mul, div, cancel, equal;
@@ -18,7 +37,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     int op1, op2;
     Boolean continueCalculationl, clearDisplay;
     String optr;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,6 +110,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             continueCalculationl = true;
         }
     }
+
+
     public void updateDisp(CharSequence str) {
         //Editable strToUpdate = (Editable)disp.getText();
         //Editable strToUpdate;
